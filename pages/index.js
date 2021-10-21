53739852763968a205aec9d0f40ecb5b0e96f155
Cooty <cooty13@gmail.com>
2021-10-21 11:21:44 +0200

    Init project

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import {Button} from "react-bootstrap";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello Bootstrap</h1>
+            <Button variant="primary">
+                My button
+            </Button>
+        </div>
+    )
+}
